41d9fd4df184dc085423050dd4da73a5aab89070
dsc <1527715645@qq.com>
2019-11-09 20:14:35 +0800

    详情页面增加markdown 解析

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+import markdown
 from django.shortcuts import render, get_object_or_404
 from .models import Article
 
@@ -12,6 +13,12 @@ def index(request):
     post_list = Article.objects.all()
     return render(request, 'blog/index.html', context={"post_list": post_list
                                                        })
+
+
 def detail(request, pk):
+    # detail 视图中解析markdown
     post = get_object_or_404(Article, pk=pk)
+    post, body = markdown.markdown(
+        post.body, extensions=[
+            'markdown.extensions.extra', 'markdown.extensions.codehilite', 'markdown.extensions.toc', ])
     return render(request, 'blog/detail.html', context={'post': post})
